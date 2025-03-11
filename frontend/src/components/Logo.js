9b7b5abf06d0b5fc0515386d80ee89ecc16e96dd
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2025-03-11 13:05:57 +0530

    update home page

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logoIcons from '../assest/logo.png'
+
+const Logo = () => {
+  return (
+    <div className='w-20 h-20 mx-auto'>
+      <img src={logoIcons} alt='login icon' />
+    </div>
+  )
+}
+
+export default Logo
