1747b7e6d88dccfef74e07406594c66e0b5b3628
Nethmi Thathsarani <thathsaraninethmi942@gmail.com>
2025-03-18 18:59:28 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose")
 
-
 async function connectDB(){
     try{
         await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Mongo URI:", process.env.MONGODB_URI);
+        
     }catch(err){
-        console.log(err)
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
     }
 }
 
--- backend/model/UserModel.js
+++ backend/model/UserModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
 })
 
 
-const userModel =  mongoose.model("user",userSchema)
+const UserModel =  mongoose.model("user",userSchema)
 
 
-module.exports = userModel
+module.exports = UserModel
